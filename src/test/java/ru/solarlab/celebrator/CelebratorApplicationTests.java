623be937324fcdbefdc4e4dd304141f63bd356a6
KrolikOVSky Alex <ovskykrolik@gmail.com>
2022-01-14 00:35:29 +0300

    Add default test class

---
+++ src/test/java/ru/solarlab/celebrator/CelebratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.solarlab.celebrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelebratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

4cb62020f2a93d80c9221ab4d135fc3134b96634
KrolikOVSky Alex <ovskykrolik@gmail.com>
2022-01-14 00:34:14 +0300

    Main class for run the application

---
+++ src/main/java/ru/solarlab/celebrator/CelebratorApplication.java
@@ -0,0 +1,13 @@
+package ru.solarlab.celebrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelebratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CelebratorApplication.class, args);
+    }
+
+}

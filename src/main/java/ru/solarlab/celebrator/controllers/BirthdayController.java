620a09e23048c5a249785bff018ab5ace473fa2e
KrolikOVSky Alex <ovskykrolik@gmail.com>
2022-01-14 00:27:32 +0300

    Create controller for working with Birthday objects

---
+++ src/main/java/ru/solarlab/celebrator/controllers/BirthdayController.java
@@ -0,0 +1,66 @@
+package ru.solarlab.celebrator.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import ru.solarlab.celebrator.entity.model.in.CreateBirthdayModel;
+import ru.solarlab.celebrator.entity.model.in.UpdateBirthdayModel;
+import ru.solarlab.celebrator.entity.model.out.BirthdayModel;
+import ru.solarlab.celebrator.services.BirthdayService;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/")
+public class BirthdayController {
+
+    private final BirthdayService birthdayService;
+
+    public BirthdayController(BirthdayService birthdayService){
+        this.birthdayService = birthdayService;
+    }
+
+    @GetMapping
+    public List<BirthdayModel> getAllPeople() {
+        return birthdayService.getAll();
+    }
+
+    @PostMapping
+    public void create(@RequestBody CreateBirthdayModel createBirthdayModel){
+        birthdayService.createBirthday(createBirthdayModel);
+    }
+
+    @PutMapping
+    public void update(@RequestBody UpdateBirthdayModel updateBirthdayModel){
+        birthdayService.editBirthday(updateBirthdayModel);
+    }
+
+    @DeleteMapping
+    public void delete(@RequestBody Map<String, Object> body){
+        System.out.println(body.get("id"));
+        birthdayService.delete(body.get("id").toString());
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

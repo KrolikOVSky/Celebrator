23069618ed3fdbbab2831067c70992b11c9c15d7
KrolikOVSky Alex <ovskykrolik@gmail.com>
2022-01-20 01:17:57 +0300

    Add new field to CelebrateModel model

--- src/main/java/ru/solarlab/celebrator/entity/model/out/CelebrateModel.java
+++ src/main/java/ru/solarlab/celebrator/entity/model/out/CelebrateModel.java
@@ -4,11 +4,37 @@ import ru.solarlab.celebrator.entity.Celebrate;
 
 public class CelebrateModel {
     private int age;
-    private String snm;
+    private String name;
+    private String lastName;
+    private String midName;
+    private String image;
+    private String date;
+    private int mark;
 
     public CelebrateModel(Celebrate celebrate) {
         this.age = celebrate.getAge();
-        this.snm = celebrate.getSnm();
+        this.date = String.format("%d-%d-%d", celebrate.getYear(), celebrate.getMonth(), celebrate.getDay());
+        this.mark = celebrate.getMark();
+        this.name = celebrate.getName();
+        this.lastName = celebrate.getLastName();
+        this.midName = celebrate.getMidName();
+        this.image = celebrate.getImage();
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 
     public int getAge() {
@@ -19,11 +45,37 @@ public class CelebrateModel {
         this.age = age;
     }
 
-    public String getSnm() {
-        return snm;
+    public String getImage() {
+        return image;
     }
 
-    public void setSnm(String snm) {
-        this.snm = snm;
+    public void setImage(String image) {
+        this.image = image;
     }
+
+    public String getMidName() {
+        return midName;
+    }
+
+    public void setMidName(String midName) {
+        this.midName = midName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
